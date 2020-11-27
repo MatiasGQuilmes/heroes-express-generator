a20cc636eead27c425a9f755042e085fb20956de
Lucas Cardozo <ishida393@gmail.com>
2020-11-27 15:07:51 -0300

    en proceso

--- controllers/heroeController.js
+++ controllers/heroeController.js
@@ -1,9 +1,8 @@
-const { RSA_NO_PADDING } = require('constants');
 const fs = require('fs')
 heroes = JSON.parse(fs.readFileSync('./data/heroes.json', 'utf-8'));
 
 module.exports =  {
     heroes : (req, res)=>{
-        res.render
+        res.render('heroes', {title : heroes})
     }
 }
